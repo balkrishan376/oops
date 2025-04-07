ca0fa5dd5adcbbca899be40c13533f2e974e1b1b
balkrishan376 <tbalkrishan603@gmail.com>
2025-04-07 15:19:00 +0530

    Add files via upload

---
+++ AddVariable.java
@@ -0,0 +1,20 @@
+public class AddVariable {
+    public static void main(String[] args) {
+        String firstName = "Balkrishan";
+        String lastName = "Thakur";
+        String fullName = firstName+lastName;
+        int x=4, y=9,z=2;
+        int age=21;
+        String clgName="GLBITM";
+        String add="Noida";
+
+        System.out.println(fullName);
+        System.out.println(firstName + lastName);
+        System.out.println(5*9);
+        System.out.println(x+y+z);
+        System.out.println("My name is: "+ fullName);
+        System.out.println("My age is: "+ age);
+        System.out.println("My college name is: "+clgName);
+        System.out.println("My address is: "+add);
+    }
+}
